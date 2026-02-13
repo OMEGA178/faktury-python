83a4b008f6c105f5bd703baaa5c00798d534b72b
Dawid Gadowski <gadowskispzoo@gmail.com>
2026-02-13 13:06:47 +0100

    Poprawka instalatora - usuniety --silent i call, widoczne pelne komunikaty bledow

--- INSTALUJ.bat
+++ INSTALUJ.bat
@@ -32,23 +32,28 @@ echo OK - Node.js zainstalowany: %NODE_VERSION%
 :: Instaluj pakiety
 echo.
 echo Krok 2/3: Instaluje pakiety (moze trwac kilka minut)...
-call npm install --silent
+echo.
+npm install
 if %errorLevel% neq 0 (
     echo.
     echo BLAD podczas instalacji!
+    echo Sprawdz powyzsze komunikaty.
+    echo.
     pause
     exit /b 1
 )
+echo.
 echo OK - Pakiety zainstalowane
 
 :: Uruchom aplikacje
 echo.
 echo Krok 3/3: Uruchamiam aplikacje...
 echo.
+echo.
 echo Aplikacja bedzie dostepna pod adresem:
 echo http://localhost:5174
 echo.
-echo Otworzysz automatycznie w przegladarce za 3 sekundy...
+echo Otwiera sie automatycznie w przegladarce za 3 sekundy...
 echo.
 timeout /t 3 >nul
 start "" http://localhost:5174
@@ -56,4 +61,4 @@ echo.
 echo Aplikacja dziala. NIE ZAMYKAJ tego okna!
 echo Aby zatrzymac aplikacje, zamknij to okno.
 echo.
-call npm run dev
+npm run dev
